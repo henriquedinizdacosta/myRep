92a750d176512ee00c7d236734245282f4bc78df
Henrique Diniz da Costa <86133112+henriquedinizdacosta@users.noreply.github.com>
2024-08-06 09:01:08 -0300

    Update flettest.py

--- Projects/flettest.py
+++ Projects/flettest.py
@@ -3,8 +3,9 @@ from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
 from flet_core.control_event import ControlEvent
 
 
-def main(page: ft.Page) -> None:
+def main(page: ft.Page):
 
+    # window settings
     page.title = 'Signup'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.theme_mode = ft.ThemeMode.LIGHT
@@ -12,40 +13,57 @@ def main(page: ft.Page) -> None:
     page.window_height = 400
     page.resizable = False
 
-    text_username: TextField = TextField(label='Username', text_align=ft.TextAlign.LEFT, width=200)
-    text_password: TextField = TextField(label='Password', text_align=ft.TextAlign.LEFT, width=200, password=True)
-    checkbox_signup: Checkbox = Checkbox(label='I agreeolapijuwlkjgfs', value=False)
-    button_submit: ElevatedButton = ElevatedButton(text='Submit', width=200, disabled=True)
+    # window objects
+    text_username = TextField(             # user field
+                        label='Username', 
+                        text_align=ft.TextAlign.LEFT, 
+                        width=200)
+    text_password = TextField(             # pass field
+                        label='Password', 
+                        text_align=ft.TextAlign.LEFT, 
+                        width=200, password=True)
+    checkbox_signup = Checkbox(            # agreement box
+                        label='I agree to paying 1b dollars to the owner', 
+                        value=False)
+    button_submit = ElevatedButton(        # submit button
+                        text='Submit', 
+                        width=200, 
+                        disabled=True)
 
-    def validate(e: ControlEvent) -> None: 
+    # check inputs to enable submit button
+    def validate(e: ControlEvent):
         if all([text_username.value, text_password.value, checkbox_signup.value]):
             button_submit.disabled = False
-        else: button_submit.disabled = True
+        else: 
+            button_submit.disabled = True  
+        page.update()                               # set button -> refresh window
 
-        page.update()
-
-    def submit(e: ControlEvent) -> None:
-        print('Username: ', text_username.value)
-        print('Password: ', text_password.value)
-
-        page.clean()
+    # submit button is pressed
+    def submit(e: ControlEvent):
+        print('Username: ', text_username.value)    # display data on terminal
+        print('Password: ', text_password.value)    # display data on terminal
+        page.clean()                           
         page.add(
             Row(
-                controls=[Text(value=f'Welcome: {text_username.value}', size=20)],
+                controls=[Text(value=f'Welcome: {text_username.value}',size=20)],
                 alignment=ft.MainAxisAlignment.CENTER
             )
         )
 
-    checkbox_signup.on_change = validate
+    # call functions on onject change
     text_username.on_change = validate
     text_password.on_change = validate
+    checkbox_signup.on_change = validate 
     button_submit.on_click = submit
 
+    # add objects
     page.add(
         Row(
-            controls=[Column([text_username,text_password,checkbox_signup,button_submit])],
+            controls=[Column([text_username, text_password, checkbox_signup, button_submit])],
             alignment=ft.MainAxisAlignment.CENTER
         )
-    )
+    ) 
 
+
+# start flet app with main()
 ft.app(target=main)
